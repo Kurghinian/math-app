7ad80952398c66fcce2e595d878a6bdf31cd3c35
Kurghinian <84278266+Kurghinian@users.noreply.github.com>
2021-05-25 23:58:55 +0400

    Add files via upload

---
+++ math.py
@@ -0,0 +1,13 @@
+from math import *
+op = input()
+if op == "cos":
+	num = int(input())
+	print(cos(num))
+elif op == "sin":
+	num = int(input())
+	print(sin(num))
+elif op == "tg":
+	num = int(input())
+	print(tan(num))
+elif op == "pi":
+	print(pi)
